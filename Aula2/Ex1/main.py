7fac96345c466be2d69659b516dda6692de6e529
RSiopa <siopainacio98@ua.pt>
2021-10-17 11:46:59 +0100

    first commit

---
+++ Aula2/Ex1/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def main():
+
+    students = ['Lucas', 'Manuel']
+
+    for student in students:
+        print('Hello ' + ' name: ' + student)
+
+if __name__ == "__main__":
+    main()
