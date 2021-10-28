e246b3149bbd8b2685443ac641e2f305b1272a8a
RSiopa <siopainacio98@ua.pt>
2021-10-28 15:04:27 +0100

    Part 4

--- Aula4/main.py
+++ Aula4/main.py
@@ -6,19 +6,39 @@
 # --------------------------------------------------
 
 import argparse
+import math
+
+import readchar
+from time import time
 
 parser = argparse.ArgumentParser(description='Definition of test mode')
-parser.add_argument('-utm', '--use_time_mode', help='The test runs for X seconds', action='store_true')
-parser.add_argument('-mv', '--max_value', type=int, help='The test runs for MAX_VALUE inputs')
+parser.add_argument('-utm', '--use_time_mode', help='The test runs for MAX_VALUE seconds\n ', action='store_true')
+parser.add_argument('-mv', '--max_value', type=int, required=True, help='The test runs for MAX_VALUE inputs\n ')
 args = parser.parse_args()
 
 
 def main():
-    # print(args.max_value)
+
     if args.use_time_mode:
-        print('hi')
-    if type(args.max_value) == int:
-        print('bye')
+        tstop = args.max_value      #max time set
+        N = math.inf
+    else:
+        tstop = math.inf
+        N = args.max_value      #max inputs set
+
+    print('Pressione uma tecla para começar.')
+    readchar.readchar()
+    t1 = time()
+    t2 = time()
+    count = 0
+
+    while (count < N) and (t2 - t1 < tstop):
+        a = t2 - t1
+        print(a)
+        readchar.readchar()
+        count = count + 1
+        t2 = time()
+
 
 
 if __name__ == '__main__':
