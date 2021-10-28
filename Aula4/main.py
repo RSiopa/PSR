23d62353aeae8932d1627d05c013fe34443d126a
RSiopa <siopainacio98@ua.pt>
2021-10-28 12:21:28 +0100

    Part 4

---
+++ Aula4/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.8
+# --------------------------------------------------
+# A python script for complex number operations using named tuples and classes
+# Rafael Inacio Siopa.
+# PSR, November 2021.
+# --------------------------------------------------
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Definition of test mode')
+parser.add_argument('-utm', '--use_time_mode', help='The test runs for X seconds')
+parser.add_argument('-mv', '--max_value', help='The test runs for MAX_VALUE inputs')
+args = parser.parse_args()
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
