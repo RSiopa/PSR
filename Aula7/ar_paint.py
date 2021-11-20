d915b2b0c96770f4dceb98a26f513e02fd0c5b33
RSiopa <siopainacio98@ua.pt>
2021-11-20 14:35:36 +0000

    Advanced Functionality 2 Finally Done

--- Aula7/ar_paint.py
+++ Aula7/ar_paint.py
@@ -1,21 +1,48 @@
-#!/usr/bin/python3.8
+#!/usr/bin/env python3
 # --------------------------------------------------
 # Python script with a variety of painting functions!
 # Rafael Inacio Siopa.
 # Rodrigo Dinis Martins Ferreira.
-# Frederico Ribeiro e Martins.
 # Bartosz Bartosik.
+# Frederico Ribeiro e Martins.
 # PSR, November 2021.
 # --------------------------------------------------
 import argparse
 import copy
+from datetime import datetime
+from functools import partial
+
 from numpy import zeros
 from color_segmenter import *
 
+drawing = False
+ix = -1
+iy = -1
+
 # Argument
-parser = argparse.ArgumentParser(description='Definition of test mode')
+parser = argparse.ArgumentParser()
 parser.add_argument('-j', '--json', type=str, help='Full path to json file.\n ')
+parser.add_argument('-usp', '--use_shake_prevention', action='store_true', help='When activated prevents random '
+                                                                                'scribbles due to fast movement.\n ')
 args = vars(parser.parse_args())
+
+# Mouse function
+def MouseCoord(event, x, y, flags, params, window_name, img, color, thickness):
+    global drawing, ix, iy
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix = x
+        iy = y
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            cv2.line(img, (ix, iy), (x, y), color, thickness)
+            ix = x
+            iy = y
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.line(img, (ix, iy), (x, y), color, thickness)
 
 
 def main():
@@ -32,17 +59,20 @@ def main():
     window_name = 'window_sketch'
     cv2.namedWindow(window_name)
     capture = cv2.VideoCapture(0)
-    capture.set(cv2.CAP_PROP_FPS, 15)
 
     # White image aka image_sketch created
     _, image = capture.read()
     h = len(image)
     w = len(image[0])
     image_sketch = zeros([h, w, 3])
+    image_sketch2 = zeros([h, w, 3])
+    image_over_sketch = zeros([h, w, 3])
 
     for y in range(h):
         for x in range(w):
             image_sketch[y, x] = [255, 255, 255]
+            image_sketch2[y, x] = [255, 255, 255]
+            image_over_sketch[y, x] = [255, 255, 255]
 
     # Mins and maxs acquired from dictionary in Json file
     mins = np.array([limits['B']['min'], limits['G']['min'], limits['R']['min']])
@@ -53,6 +83,28 @@ def main():
 
     # Flag for the program to know that the user just started to draw a new line (lines can be disconnected)
     flag_newline = 1
+
+    print('\nPress r to change to red color.'
+          '\nPress g to change to green color.'
+          '\nPress b to change to blue color.'
+          '\nPress + to increase the thickness of the pencil'
+          '\nPress - to decrease the thickness of the pencil'
+          '\nPress m to use the mouse as the pencil.'
+          '\nPress v to change the white board to the video stream.'
+          '\nPress c to clear the sketch'
+          '\nPress w to save the sketch'
+          '\nInitializing with red color as default.')
+
+    color = (0, 0, 255)       #Default color for the sketch
+    thickness = 2             #Default thickness of the pencil
+    cX_past = 0
+    cY_past = 0
+    cX = 0
+    cY = 0
+
+    flag_mouse = 0
+
+    flag_video = 0
 
     # -----------------------------------------------------------
     # Continuous Operation
@@ -86,18 +138,22 @@ def main():
             cv2.line(image_origin, (int(cX)-10, int(cY)), (int(cX)+10, int(cY)), (0, 0, 255), 2)
             cv2.line(image_origin, (int(cX), int(cY)-10), (int(cX), int(cY)+10), (0, 0, 255), 2)
 
-            # If it is a new line, paint a circle in the centroid of the object
-            if flag_newline == 1:
-                cv2.circle(image_sketch, (int(cX), int(cY)), 0, (0, 0, 255), 2)
-                cX_past = cX
-                cY_past = cY
-                flag_newline = 0
+            if flag_mouse == 0:
+                # If it is a new line, paint a circle in the centroid of the object
+                if flag_newline == 1 and args['use_shake_prevention']:
+                    cv2.circle(image_sketch, (int(cX), int(cY)), 0, color, thickness)
+                    cv2.circle(image_sketch2, (int(cX), int(cY)), 0, color, thickness)
+                    cX_past = cX
+                    cY_past = cY
+                    flag_newline = 0
 
-            # If it is a continuation of a line, draw a line between the new centroid and the last centroid
-            else:
-                cv2.line(image_sketch, (int(cX_past), int(cY_past)), (int(cX), int(cY)), (0, 0, 255), 2)
-                cX_past = cX
-                cY_past = cY
+                # If it is a continuation of a line, draw a line between the new centroid and the last centroid
+                else:
+                    cv2.line(image_sketch, (int(cX_past), int(cY_past)), (int(cX), int(cY)), color, thickness)
+                    cv2.line(image_sketch2, (int(cX_past), int(cY_past)), (int(cX), int(cY)), color, thickness)
+                    cX_past = cX
+                    cY_past = cY
+
 
         # If no objects are present, reset the flag for a new line
         else:
@@ -107,11 +163,62 @@ def main():
         cv2.imshow('window_mask', mask)
         cv2.imshow('window_mask_largest', mask_largest)
         cv2.imshow('window_origin', image_origin)
-        cv2.imshow(window_name, image_sketch)
 
-        #Stop the program when 'q' is pressed
-        if cv2.waitKey(1) == ord('q'):
+        # indices = np.where(image_sketch2 == 255)
+        # image_over_sketch = copy.copy(image_sketch2)
+        # image_over_sketch[indices[0], indices[1], :] = image[indices[0], indices[1], :].copy()
+        # image_sketch2[indices[0], indices[1], :] = image[indices[0], indices[1], :].copy()
+        cv2.imshow(window_name, image_sketch2)
+        print(image_sketch2[50, 50, :])
+
+        if flag_video == 0:
+            cv2.imshow(window_name, image_sketch)
+        else:
+            indices = np.where(image_sketch2 == [255, 255, 255])
+            image_over_sketch = copy.copy(image_sketch2)
+            image_over_sketch[indices[0], indices[1], :] = [255, 0, 0]
+            # image_sketch2[indices[0], indices[1], :] = image[indices[0], indices[1], :].copy()
+            # # image_sketch2[np.where(image_sketch2 == 255)] = image[np.where(image_sketch2 == 255)]
+            # print(image_over_sketch[50, 50, :])
+            cv2.imshow(window_name, image_over_sketch)
+
+        # Changes the mouse color and thickness
+        MouseCoord_paint = partial(MouseCoord, window_name=window_name, img=image_sketch, color=color,
+                                   thickness=thickness)
+        # Changes modes if flag_mouse changes
+        if flag_mouse == 1:
+            cv2.setMouseCallback(window_name, MouseCoord_paint)
+        else:
+            cv2.setMouseCallback(window_name, lambda *args: None)
+
+        key = cv2.waitKey(1)
+
+        if key == ord('q'):                    # Stops the program when 'q' is pressed
             break
+        if key == ord('r'):                    # Changes the pencil color to red when 'r' is pressed
+            color = (0, 0, 255)
+        if key == ord('g'):                    # Changes the pencil color to green when 'g' is pressed
+            color = (0, 255, 0)
+        if key == ord('b'):                    # Changes the pencil color to blue when 'b' is pressed
+            color = (255, 0, 0)
+        if key == ord('+'):                    # Increases the pencil thickness when '+' is pressed
+            thickness += 1
+        if key == ord('-') and thickness > 1:  # Decreases the pencil thickness when '-' is pressed
+            thickness -= 1
+        if key == ord('m'):                    # Switches between using the mouse or not to paint when 'm' is pressed
+            flag_mouse = not flag_mouse
+        if key == ord('v'):                    # Switches between using the blank image and the video stream to paint
+                                               # when 'm' is pressed
+            flag_video = not flag_video
+            # flag_video = 1
+        if key == ord('c'):                    # Clears the sketch when 'c' is pressed
+            for y in range(h):
+                for x in range(w):
+                    image_sketch[y, x] = [255, 255, 255]
+        if key == ord('w'):                    # Saves the sketch when 'w' is pressed
+            filename = datetime.now().strftime('drawing_'+"%a_%b_%d_%H:%M:%S_%Y"+'.jpg')
+            cv2.imwrite(filename, image_sketch)
+            print(filename + ' saved.')
 
 
 if __name__ == '__main__':
