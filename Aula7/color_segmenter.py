90d98e2b5d091ee0edd8fec7052d87aeb866e5a2
RSiopa <siopainacio98@ua.pt>
2021-11-22 23:15:06 +0000

    even more updates

--- Aula7/color_segmenter.py
+++ Aula7/color_segmenter.py
@@ -2,9 +2,9 @@
 # --------------------------------------------------
 # Python script for color adjustment and saving the values to a json file.
 # Rafael Inacio Siopa.
-# Frederico Ribeiro e Martins.
 # Rodrigo Dinis Martins Ferreira.
 # Bartosz Bartosik.
+# Frederico Ribeiro e Martins.
 # PSR, November 2021.
 # --------------------------------------------------
 import json
@@ -17,25 +17,23 @@ def nothing(x):
 
 
 def main():
-    window_name = 'window - Ex3a'
+    window_name = 'Segmented'
     cv2.namedWindow(window_name)
     capture = cv2.VideoCapture(0)
-    capture.set(cv2.CAP_PROP_FPS, 15)
 
-    ranges = {'limits': {'H': {'max': 255, 'min': 0},
-                         'S': {'max': 255, 'min': 0},
-                         'V': {'max': 255, 'min': 0}}}
+    ranges = {'limits': {'B': {'max': 255, 'min': 0},
+                         'G': {'max': 255, 'min': 0},
+                         'R': {'max': 255, 'min': 0}}}
 
-    cv2.createTrackbar('min H', window_name, 0, 255, nothing)
-    cv2.createTrackbar('max H', window_name, 255, 255, nothing)
-    cv2.createTrackbar('min S', window_name, 0, 255, nothing)
-    cv2.createTrackbar('max S', window_name, 255, 255, nothing)
-    cv2.createTrackbar('min V', window_name, 0, 255, nothing)
-    cv2.createTrackbar('max V', window_name, 255, 255, nothing)
+    cv2.createTrackbar('min B', window_name, 0, 255, nothing)
+    cv2.createTrackbar('max B', window_name, 255, 255, nothing)
+    cv2.createTrackbar('min G', window_name, 0, 255, nothing)
+    cv2.createTrackbar('max G', window_name, 255, 255, nothing)
+    cv2.createTrackbar('min R', window_name, 0, 255, nothing)
+    cv2.createTrackbar('max R', window_name, 255, 255, nothing)
 
     while True:
         _, image = capture.read()
-        image_HSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
         mins = np.array([ranges['limits']['B']['min'], ranges['limits']['G']['min'], ranges['limits']['R']['min']])
         maxs = np.array([ranges['limits']['B']['max'], ranges['limits']['G']['max'], ranges['limits']['R']['max']])
@@ -47,16 +45,19 @@ def main():
         ranges['limits']['G']['max'] = maxs[1] = cv2.getTrackbarPos('max G', window_name)
         ranges['limits']['R']['max'] = maxs[2] = cv2.getTrackbarPos('max R', window_name)
 
-        mask_black = cv2.inRange(image_HSV, mins, maxs)
-        cv2.imshow(window_name, mask_black)
+        segmented = cv2.inRange(image, mins, maxs)
+        cv2.imshow(window_name, segmented)
+        cv2.imshow('Original', image)
 
-        if cv2.waitKey(1) == ord('w'):
+        key = cv2.waitKey(20)
+
+        if key == ord('w'):
             file_name = 'limits.json'
             with open(file_name, 'w') as file_handle:
                 print('writing dictionary ranges to file ' + file_name)
                 json.dump(ranges, file_handle)  # d is the dictionary
 
-        if cv2.waitKey(1) == ord('q'):
+        if key == ord('q'):
             break
 
 
